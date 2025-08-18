5ed30d424bac4e7994df3c3869afdb0f2a6e59d6
asheesh859 <ak2305@checkboxtechnology.com>
2025-08-18 16:57:23 +0530

    test one testCase 18/08/2025 4:57 PM

---
+++ unittest1/src/main/java/com/unittest1/model/Calculator.java
@@ -0,0 +1,13 @@
+package com.unittest1.model;
+
+public class Calculator{
+	
+	public int add(int x, int y) {
+		return x+y;
+	}
+	
+	public int mul(int x , int y) {
+		return x*y;
+	}
+
+}
---
+++ unittest1/src/test/java/com/unittest1/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.unittest1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.unittest1.model.Calculator;
+
+@SpringBootTest
+public class CalculatorTest {
+	
+	@Test
+	public void testAdd() {
+		int ExpectedValue = 50;
+		Calculator c = new Calculator();
+		int ActualValue = c.add(10,40);
+		Assertions.assertEquals(ExpectedValue , ActualValue);
+		
+	}
+
+}
