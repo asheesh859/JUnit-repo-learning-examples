827d2af3dc44afbc74d8413c083a0e2b197262d7
asheesh859 <ak2305@checkboxtechnology.com>
2025-08-19 17:00:19 +0530

    adding mvc controller 19/08/2025 5:00 PM

---
+++ mvc/src/main/java/com/mvc/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.mvc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mvc.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	private final UserService userService;
+	
+	public UserController(UserService userService){
+		this.userService = userService;
+	}
+	@GetMapping("/{id}")
+	public String getUserName(@PathVariable int id) {
+		return userService.getUserNameById(id);
+	}
+	
+	
+}
