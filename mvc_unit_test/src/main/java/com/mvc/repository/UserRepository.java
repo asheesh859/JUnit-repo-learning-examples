f92c469a2635005ce8ad833a141624a3ac449e46
asheesh859 <ak2305@checkboxtechnology.com>
2025-08-23 16:34:51 +0530

    add service and controller layer 23/08/2025 4:34 PM

---
+++ mvc_unit_test/src/main/java/com/mvc/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.mvc.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mvc.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	Optional<User> findById(int id);
+	User save(User user);
+	void deleteById(int id);
+	List<User> findAll(); 
+}
