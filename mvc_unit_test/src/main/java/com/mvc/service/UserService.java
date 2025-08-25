fbd15afae3270775fd76af64acb944b213ce667e
asheesh859 <ak2305@checkboxtechnology.com>
2025-08-25 23:50:56 +0530

    add function in service 25/08/2025 11:50 PM

--- mvc_unit_test/src/main/java/com/mvc/service/UserService.java
+++ mvc_unit_test/src/main/java/com/mvc/service/UserService.java
@@ -1,6 +1,8 @@
 package com.mvc.service;
 
 import com.mvc.repository.UserRepository;
+import com.mvc.entity.User;
+import java.util.Optional;
 
 public class UserService {
 	
@@ -8,6 +10,27 @@ public class UserService {
 	
 	public UserService(UserRepository userRepository) {
 		this.userRepository = userRepository;
+	}
+
+	public String getUserName(int id){
+		return userRepository.findById(id)
+				.map(user -> user.getName())
+				.orElse("User not found");
+	}
+
+	public User createUser(User user){
+		return userRepository.save(user);
+	}
+	public Optional<User> updateUser(User user, int id){
+		return userRepository.findById(id).map(existing -> userRepository.save(new User(id ,user.getName())));
+	}
+
+	public boolean deleteUser(int id){
+		if(userRepository.findById(id).isPresent()) {
+			userRepository.deleteById(id);
+			return true;
+		}
+		return false;
 	}
 	
 	
